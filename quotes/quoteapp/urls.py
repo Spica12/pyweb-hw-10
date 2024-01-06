9c992b3b47a4b389aa4ac2d262c60e781823e841
Vitalii Savenko <vitaly.savenko12@gmail.com>
2024-01-06 22:38:41 +0200

    _

---
+++ quotes/quoteapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .import views
+
+app_name = 'quoteapp'
+
+urlpatterns = [
+    path('', views.main, name='main'),
+    path('tag/', views.tag, name='tag'),
+    path('quote/',views.quote, name='quote'),
+]
--- quotes/quoteapp/views.py
+++ quotes/quoteapp/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import TagForm, QuoteForm
+from .models import Tag, Quote
+
 
 # Create your views here.
+def main(request):
+    return render(request, 'quoteapp/index.html')
+
+
+def tag(request):
+    if request.method == 'POST':
+        form = TagForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='quoteapp:main')
+        else:
+            return render(request, 'quoteapp/tag.html', {'form': form})
+
+    return render(request, 'quoteapp/tag.html', {'form': TagForm()})
+
+
+def quote(request):
+    tags = Tag.objects.all()
+
+    if request.method == 'POST':
+        form = QuoteForm(request.POST)
+        if form.is_valid():
+            new_quote = form.save()
+
+            choice_tags = Tag.objects.filter(name__in=request.POST.getlist('tags'))
+            for tag in choice_tags.iterator():
+                new_quote.tags.add(tag)
+
+            return redirect(to='quoteapp:main')
+        else:
+            return render(request, 'quoteapp/quote.html', {"tags": tags, 'form': form})
+
+    return render(request, 'quoteapp/quote.html', {"tags": tags, 'form': QuoteForm()})
